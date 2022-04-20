0b9003b1ff21ff9b3fe507acaf0b1e778b5f1420
325972 <325972@student.mboutrecht.nl>
2022-04-20 10:30:57 +0200

    Create reservation function added

--- app/controllers/Students.php
+++ app/controllers/Students.php
@@ -3,12 +3,49 @@ class Students extends Controller
 {
   public function __construct()
   {
-    $this->studentModel = $this->model('Student');
+    $this->userModel = $this->model('Student');
   }
 
   public function index()
   {
-    $this->view('student/index');
+    $this->view('students/index');
   }
-  
+
+  public function request()
+  {
+    //Default value
+    $data = null;
+    // Check if POST is send
+    if (isset($_POST)) 
+      {
+        // Check if submit button is pressed
+        if (isset($_POST["submit"])) 
+        {
+          // Put post values into variables
+          $product = $_POST["inputProduct"];
+          $amount = $_POST["inputAmount"];
+          $reason = $_POST["inputReason"];
+
+          //Check if variables are not empty
+          if (!empty($product) && !empty($amount) && !empty($reason)) 
+          {
+            //Initiate requestArticle function
+            $message = $this->userModel->requestArticle($product, $amount, $reason);
+
+            if ($message = "success") {
+              //Gelukt
+              $data = "<h1>".$message."</h1>";
+            } else {
+              //Gefaald
+              $data = "<h1>".$message."</h1>";
+            }
+              
+          }
+        }
+        
+
+}
+
+    $this->view('students/index', $data);
+  }
 }
