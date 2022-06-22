e8cef29b255d0158971fcecbfa50959c5f8b1919
MaxPuddu <71008325+MaxPuddu@users.noreply.github.com>
2022-06-22 10:04:46 +0200

    Addition
    Created a config file for the lesrooster mvc code

---
+++ Config.php
@@ -0,0 +1,21 @@
+<?php
+    //Database gegevens van mysql
+    
+    define('DB_HOST', 'localhost'); //Database host
+    define('DB_USER', 'root'); // Hier de gebruiker van de databse
+    define('DB_PASS', ''); //Hier een database wachtwoord
+    define('DB_NAME', 'rijles'); //Hier de database naam
+
+
+    //APPROOT
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    //URLROOT (Dynamic links)
+    define('URLROOT', 'http://www.rijles.nl');
+
+    //Sitename
+    define('SITENAME', 'Lesrooster');
+
+    define('SQLROOT', '../app/Sqlscripts/');
+
+    
