281049a002cf187b03154701d06e1506b1bc2928
yusuf <321611@student.mboutrecht.com>
2022-06-27 16:53:21 +0200

    dit is een commit na de examen voor de presentatie met bij gevoegde commentaars

--- app/config/config.php
+++ app/config/config.php
@@ -1,12 +1,12 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'YusufExamen');
-define('DB_PASS', 'Sanane68');
-define('DB_NAME', 'examen2022');
+define('DB_USER', 'YusufExamen'); //Gebruikers naam
+define('DB_PASS', 'Sanane68'); //wachtwoord van de gebruiker
+define('DB_NAME', 'examen2022'); //databasa naam 
 
 define('APPROOT', dirname(dirname(__FILE__))) ;
 
-define('URLROOT', 'http://www.easydrive4all.org');
+define('URLROOT', 'http://www.easydrive4all.org'); //website url
+define('SITENAME', 'easydrive4all');
 
-define('SITENAME', 'Easydrive4all');
 ?>
